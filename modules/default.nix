e3398e575560c1403cbc6e3b89addd76e9b9a94e
eveeifyeve <kbkk7250@outlook.com>
2026-07-25 09:22:53 +1000

    portable-lib: migrate to use nixpkgs

--- modules/default.nix
+++ modules/default.nix
@@ -6,10 +6,11 @@
   ...
 }:
 let
-  portable-lib = import ./portable/lib.nix { inherit lib; };
-
+  portable-lib = lib.services;
   modularServiceConfiguration = portable-lib.configure {
-    serviceManagerPkgs = pkgs;
+    baseModules = [
+      (lib.modules.importApply pkgs.path /lib/services/service.nix { inherit pkgs; })
+    ];
     extraRootModules = [
       ./finit/service.nix
     ];
