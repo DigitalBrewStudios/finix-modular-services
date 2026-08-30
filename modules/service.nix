c27f848b170749ff6f1a06d554d0ea6354712d52
eveeifyeve <kbkk7250@outlook.com>
2026-08-31 03:32:10 +1000

    modular-services: overriding support to finix command

--- modules/service.nix
+++ modules/service.nix
@@ -14,6 +14,32 @@ in
       '';
     };
 
+	finit.command = mkOption {
+		type = types.str;
+		default = lib.escapeShellArgs config.process.argv;
+		defaultText = lib.literalExpression "lib.escapeShellArgs config.process.argv";
+		description = ''
+				Starting Command for finix.
+
+				This option sets the primary starting entry, and is the way to extend the
+				command line derived from {option}`process.argv`.
+
+				By default, it is set to `lib.escapeShellArgs {option}process.argv`. Because
+				{option}`process.argv` is already a command line (not an argument
+				list), it is used verbatim so that references like `$MAINPID` are preserved.
+				When {option}`process.argv` is unset, this option is `null` and no
+				`finit.command` is emitted; a service may then set
+				`finit.command` itself.
+
+				To extend {option}`process.argv`, you can append
+				to the command line:
+				```nix
+				finit.command =
+					config.process.argv + "<example>";
+				```
+			'';
+		};
+
     # Import this logic into sub-services also.
     # Extends the portable `services` option.
     services = lib.mkOption {
@@ -30,7 +56,8 @@ in
 
   config = {
     finit.services."" = {
-      command = lib.escapeShellArgs config.process.argv;
+      command = config.finit.command;
     };
+
   };
 }
