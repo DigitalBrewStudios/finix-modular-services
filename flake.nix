563af8043ba1edbf02379c2aa10e6b29ae963e47
eveeifyeve <kbkk7250@outlook.com>
2026-09-05 03:22:07 +1000

    modular-services: tests init

--- flake.nix
+++ flake.nix
@@ -1,7 +1,79 @@
 {
   description = "Support for Modular services in finix";
-
-  outputs = _: {
-    nixosModules.default = import ./modules/service.nix;
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    finix.url = "github:finix-community/finix";
   };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      finix,
+    }:
+    let
+      finixSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+
+      forAllFinixSystems =
+        f:
+        nixpkgs.lib.genAttrs finixSystems (
+          system:
+          let
+            pkgs = import nixpkgs { inherit system; };
+          in
+          f pkgs system
+        );
+    in
+    {
+      nixosModules.default = import ./modules/service.nix;
+
+      checks = forAllFinixSystems (
+        pkgs: system: {
+          default = pkgs.testers.modularServiceCompliance {
+            callReload = path: "initctl reload ${path}";
+            sharedDir = ".finix-test";
+            evalConfig =
+              { services, ... }:
+              let
+                machine = finix.lib.finixSystem {
+                  inherit (pkgs) lib;
+                  modules = [
+                    {
+                      nixpkgs.pkgs = nixpkgs.lib.mkDefault pkgs;
+                    }
+                    {
+                      system.services = services;
+                    }
+                    self.nixosModules.default
+                  ];
+                };
+              in
+              {
+                config = machine.config.system.services;
+                checkDrv = machine.config.system.build.toplevel;
+              };
+            mkTest =
+              {
+                name,
+                services,
+                testExe,
+              }:
+              finix.lib.mkTest {
+                inherit name;
+                nodes.machine = {
+                  imports = [ self.nixosModules.default ];
+                  system.services = services;
+                };
+                testScript = ''
+                  machine.wait_for_condition("service/syslogd/ready")
+                  machine.succeed("${testExe}")
+                '';
+              };
+          };
+        }
+      );
+    };
 }
